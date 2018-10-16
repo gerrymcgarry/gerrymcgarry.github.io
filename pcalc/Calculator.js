967736bfa5571553cd9e910215a4051d2cd45d74
Gerry McGarry <gerrymcgarry@gmail.com>
2018-10-16 15:58:57 +0100

    Added more functionality

--- pcalc/Calculator.js
+++ pcalc/Calculator.js
@@ -1,46 +1,87 @@
 class Calculator  {
     constructor () {
-        this.display = "";
+        this.display = "0";
         this.firstNumber = 0;
         this.memory = 0;
         this.function = "";
         this.screen = document.getElementById('display');
     }
+    updateDisplay() { displayScreen.textContent = calculator.display; } 
+
+    updateDisplay2(value) {
+        this.display = value;
+        displayScreen.textContent = calculator.display;
+    } 
+
+    equals() {
+        calculator[calculator.function](parseFloat(calculator.display));
+        this.firstNumber = 0;
+        this.function = ""; 
+    }
 
     // Single Functions
-    memoryRecall() { this.display = this.memory;  updateDisplay(); };
-    memoryPlus() { this.memory += parseFloat(this.display); console.log("+" + this.display);};
+    memoryRecall() { this.display = this.memory;  this.updateDisplay(); };
+    memoryPlus() { this.memory += parseFloat(this.display); };
     memoryMinus() { this.memory -= parseFloat(this.display); };
     memoryClear() { this.memory = 0;};
     play() { document.getElementById("audio").play(); };
 
-    sin(number) { this.display = Math.round(Math.sin(number) * 10000000) / 10000000; };
-    cos(number) { this.display = Math.round(Math.cos(number) * 10000000) / 10000000; };
-    tan(number) { this.display = Math.round(Math.tan(number) * 10000000) / 10000000; };
+    sin(number) { this.display = Math.round(Math.sin(number*Math.PI/180) * 10000000) / 10000000; };
+    cos(number) { this.display = Math.round(Math.cos(number*Math.PI/180) * 10000000) / 10000000; };
+    tan(number) { this.display = Math.round(Math.tan(number*Math.PI/180) * 10000000) / 10000000; ;};
+    asin(number) { this.display = Math.round(Math.asin(number)*(180/Math.PI) * 10000000) / 10000000; };
+    acos(number) { this.display = Math.round(Math.acos(number)*(180/Math.PI) * 10000000) / 10000000; };
+    atan(number) { this.display = Math.round(Math.atan(number)*(180/Math.PI) * 10000000) / 10000000; ;};
     log(number) { this.display = Math.round(Math.log10(number) * 10000000) / 10000000; };
+    ln(number) { this.display = Math.round(Math.log(number) * 10000000) / 10000000; };
     sqr(number) { this.display = Math.round(Math.sqrt(number) * 10000000) / 10000000; };
     exp(number) { this.display = Math.round(Math.exp(number) * 10000000) / 10000000; };
-
-    pi() { this.display = "3.14158926";updateDisplay();};
-    clear() {calculator.display = "0";updateDisplay();};
+    percent(number) { this.display = Math.round(this.display * 100000) / 10000000; };
+    fac(number) {
+        if (number === 0 || number === 1)
+          return 1;
+        for (var i = number - 1; i >= 1; i--) {
+          number *= i;
+        }
+        this.display =  number;
+      }
+    pi() {  this.updateDisplay2("3.14158926");};
+    clear() { this.updateDisplay2("0"); };
     clearAll() {            
-        calculator.firstNumber = 0;
-        calculator.function = ""; 
-        calculator.display = "0";
-        updateDisplay();
+        this.firstNumber = 0;
+        this.function = ""; 
+        this.updateDisplay2("0");
     };
-
+    plusMinus() { 
+        if (this.display !== '0') {
+            if (this.display[0] !== '-') {
+                this.display = "-".concat(this.display);
+            } else {
+                this.display = this.display.substring(1, this.display.length);
+            }
+        }
+    };
+    number(el) {
+        if (this.display.length < 8) {
+            if (this.display !== "0") {
+                this.display += el.textContent;
+            } else {
+                this.display = el.textContent;
+            }
+        this.updateDisplay();
+        }
+    }
+    backspace() { if (this.display.length != 1) {
+        this.display = this.display.substring(0, this.display.length - 1); 
+    } else if (this.display !== "0") {
+        this.display = "0";
+     };
+    }
     // Double functions
-    multiply(number) { this.firstNumber = this.firstNumber * parseFloat(this.display); };
-    divide(number) { this.firstNumber = this.firstNumber / parseFloat(this.display); };
-    subtract(number) { this.firstNumber -= parseFloat(this.display); };
-    add(number) { this.firstNumber += parseFloat(this.display); };
-    pow() { this.firstNumber = Math.pow(this.firstNumber, parseFloat(this.display)); };
-    mod() { this.firstNumber = this.firstNumber % parseFloat(this.display); };
-
-
-    // To Do
-    percent() {console.log("%");};
-    backspace() {console.log("delete");};
-    plusMinus() {console.log("+/-");};
+    multiply(secondNumber) { this.display = this.firstNumber * secondNumber; };
+    divide(secondNumber) { this.display = this.firstNumber / secondNumber; };
+    subtract(secondNumber) { this.display = this.firstNumber - secondNumber; };
+    add(secondNumber) { this.display = this.firstNumber + secondNumber; };
+    pow(secondNumber) { this.display = Math.pow(this.firstNumber, secondNumber); };
+    mod(secondNumber) { this.display = this.firstNumber % secondNumber; };  
 }
--- pcalc/app.js
+++ pcalc/app.js
@@ -1,44 +1,22 @@
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js');
 }
+const calculator = new Calculator();
 
 const numberButtons = document.querySelectorAll(".number");
 const functionButtons = document.querySelectorAll(".function");
 const function2Buttons = document.querySelectorAll(".function2");
 const displayScreen = document.getElementById('display');
 
-const calculator = new Calculator();
-
-function play() {
-    var audio = document.getElementById("audio");
-    displayScreen.textContent = "KRAFTWERK";
-    audio.play();
-    setTimeout(function(){ updateDisplay(); }, 3000);
-}
-
-const updateDisplay = () => {
-    displayScreen.textContent = calculator.display;
- } 
-
 numberButtons.forEach( (el) => {
-    el.addEventListener('click', 
-        () => {
-            if (calculator.display.length < 10) {
-            calculator.display += el.textContent;
-            updateDisplay();
-            }
-        });
+    el.addEventListener('click', () => calculator.number(el) );
 });
 
 functionButtons.forEach( (el) => {
     el.addEventListener('click', () => {
-         if ( el.id == "equals" ) {
-            calculator[calculator.function](parseFloat(calculator.display));
-            calculator.display = calculator.firstNumber;
-        } else {
-            calculator[el.id](parseInt(calculator.display));
-        }
-        updateDisplay();
+        calculator[el.id](parseFloat(calculator.display));
+        calculator.updateDisplay();
     });
 });
 
@@ -46,8 +24,6 @@ function2Buttons.forEach( (el) => {
     el.addEventListener('click', () => {
         calculator.firstNumber = parseFloat(calculator.display);
         calculator.function = el.id;
-        calculator.display = "0";
-        console.log("Fun2 - " + el.id);
-        updateDisplay();
+        calculator.updateDisplay2("0");
     });
 });
