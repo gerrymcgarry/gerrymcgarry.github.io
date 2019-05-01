8d14a4a7a077e8a01a98814339be16cb4ad63909
Gerry McGarry <gerrymcgarry@gmail.com>
2019-05-01 01:48:56 +0100

    added location

--- tfl-rail/app.js
+++ tfl-rail/app.js
@@ -40,7 +40,24 @@ function initMap() {
         preserveViewport: false,
         map: map
       });
-  
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
     //   tflLayer.addListener('click', function(event) {
     //       var content = event.featureData.infoWindowHtml;
     //       var testimonial = document.getElementById('capture');
